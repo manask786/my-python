17c354d170369ebc32914a8c5c22ed46322f5d7e
manask786 <146158203+manask786@users.noreply.github.com>
2023-09-29 11:44:02 +0530

    day2

---
+++ hello.py
@@ -0,0 +1,8 @@
+print(6456464)
+print('hello')
+print("hello world")
+#print(23+56)
+a=4.5
+w=45
+print(type(a))
+print(a+w)
---
+++ task1.py
@@ -0,0 +1,3 @@
+n=int(input('enter first number = '))
+m=int(input('enter second number = '))
+print('total sum is',n+m)
